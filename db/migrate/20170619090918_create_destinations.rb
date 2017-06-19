206960151c50e4bf79f549539da7b2af9d7fb774
Romain Demoustier <rdemoustier@hotmail.com>
2017-06-19 11:10:33 +0200

    destination model created

---
+++ db/migrate/20170619090918_create_destinations.rb
@@ -0,0 +1,10 @@
+class CreateDestinations < ActiveRecord::Migration[5.1]
+  def change
+    create_table :destinations do |t|
+      t.string :name
+      t.references :user, foreign_key: true
+
+      t.timestamps
+    end
+  end
+end
