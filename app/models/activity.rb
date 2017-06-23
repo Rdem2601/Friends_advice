21bff6ea2d7c44d46992aa1846974f63085e1af5
Romain Demoustier <rdemoustier@hotmail.com>
2017-06-23 11:42:39 +0200

    geocoding implemented

--- app/models/activity.rb
+++ app/models/activity.rb
@@ -1,6 +1,9 @@
 class Activity < ApplicationRecord
   mount_uploader :photo, PhotoUploader
 
+  geocoded_by :address
+  after_validation :geocode, if: :address_changed?
+
   belongs_to :destination
 
   validates :name, presence: true
