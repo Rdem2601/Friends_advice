c21f9015ea6a324f5d6ce0425f5c4aaae8657003
Romain Demoustier <rdemoustier@hotmail.com>
2017-06-28 11:05:16 +0200

    not installed

--- app/models/restaurant.rb
+++ app/models/restaurant.rb
@@ -3,10 +3,12 @@ class Restaurant < ApplicationRecord
 
   belongs_to :destination
 
-  geocoded_by :address
-  after_validation :geocode, if: :address_changed?
-
   validates :name, presence: true
   validates :address, presence: true
   validates :review, presence: true
+
+  geocoded_by :address
+  after_validation :geocode, if: :address_changed?
+
+
 end
