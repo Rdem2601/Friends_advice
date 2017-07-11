092507ef576ba62fd8fdc04f2025f38908b0e3ab
Julien Crepin <jppcrepin@gmail.com>
2017-07-11 16:34:55 +0200

    begin the auocomplete

---
+++ app/assets/javascripts/autocomplete.js
@@ -0,0 +1,11 @@
+jQuery(function() {
+  var completer;
+
+  completer = new GmapsCompleter({
+    inputField: '#gmaps-input-address',
+    errorField: '#gmaps-error'
+  });
+
+  completer.autoCompleteInit({
+  });
+});
